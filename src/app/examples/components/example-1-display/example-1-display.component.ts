61f4499503e8a19c36a34db4795e4e159b73ae9c
Christopher J. Walker <cwalker@brainshark.com>
2018-04-28 15:14:00 -0400

    Paginator fix for navigation after data loaded.

--- src/app/examples/components/example-1-display/example-1-display.component.ts
+++ src/app/examples/components/example-1-display/example-1-display.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { MatPaginator, MatTableDataSource } from '@angular/material';
 import { ExampleModel } from '../../models/ExampleModel.model';
 
@@ -8,7 +8,7 @@ import { ExampleModel } from '../../models/ExampleModel.model';
   styleUrls: ['./example-1-display.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Example1DisplayComponent {
+export class Example1DisplayComponent implements AfterViewInit {
   displayedColumns = ['id', 'title'];
   exampleData: ExampleModel[];
   dataSource: MatTableDataSource<ExampleModel>;
@@ -23,5 +23,8 @@ export class Example1DisplayComponent {
   clearData() {
     this.clearExampleData.emit();
     this.dataSource.paginator.firstPage();
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 }
