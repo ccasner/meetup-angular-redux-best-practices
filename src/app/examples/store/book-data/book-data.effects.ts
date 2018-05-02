5ecd707ad9fd5aaadd868dfe4d8632284ad5e38d
rquimby <rquimby600@gmail.com>
2018-05-02 09:54:17 -0400

    bad examples

--- src/app/examples/store/book-data/book-data.effects.ts
+++ src/app/examples/store/book-data/book-data.effects.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs/observable/of';
-import { catchError, delay, map, switchMap } from 'rxjs/operators';
+import { catchError, delay, map, switchMap, take } from 'rxjs/operators';
 import { BookModel } from '../../models/BookModel.model';
 import { BookDataActionTypes, LoadBooks, LoadBooksError, LoadBooksSuccess } from './book-data.actions';
 
@@ -22,6 +22,21 @@ export class BookDataEffects {
       );
     }),
   );
+
+  // Bad Example 3 - don't create an effect for derived state
+  //
+/*   @Effect()
+  setBookTheme$ = this.actions$
+    .ofType<LoadBooks>(BookDataActionTypes.LoadThemeSuccess)
+    .pipe(
+      map(action => action.payload),
+      map(({ data }) => new SetBookTheme(data.theme)),
+  );
+
+  export const getBookTheme = createSelector(
+    getBookData,
+    (data) => data.theme
+  ); */
 
   constructor(private actions$: Actions, private http: HttpClient) {}
 }
